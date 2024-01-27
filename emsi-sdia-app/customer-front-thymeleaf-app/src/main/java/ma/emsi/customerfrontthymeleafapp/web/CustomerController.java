1871346352ee09f568102b1c5c819fce4e7ba128
Younes-Hounain <younesho9@outlook.com>
2024-01-27 18:14:25 +0100

    [CD] : added the login button :radio_button:

--- emsi-sdia-app/customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/web/CustomerController.java
+++ emsi-sdia-app/customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/web/CustomerController.java
@@ -4,19 +4,25 @@ import ma.emsi.customerfrontthymeleafapp.entities.Customer;
 import ma.emsi.customerfrontthymeleafapp.repository.CustomerRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class CustomerController {
     private CustomerRepository customerRepository;
+    private ClientRegistrationRepository clientRegistrationRepository;
 
-    public CustomerController(CustomerRepository customerRepository) {
+    public CustomerController(CustomerRepository customerRepository, ClientRegistrationRepository clientRegistrationRepository) {
         this.customerRepository = customerRepository;
+        this.clientRegistrationRepository = clientRegistrationRepository;
     }
 
     @GetMapping(path = "/customers")
@@ -43,6 +49,19 @@ public class CustomerController {
     @GetMapping(path = "/notAuthorized")
     public String notAuthorized() {
         return "notAuthorized";
+    }
+
+    @GetMapping("/oauth2Login")
+    public String oauth2Login(Model model) {
+        String authorizationRequestBaseUri = "oauth2/authorization";
+        Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+        Iterable<ClientRegistration> clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+        clientRegistrations.forEach(registration ->{
+            oauth2AuthenticationUrls.put(registration.getClientName(),
+                    authorizationRequestBaseUri + "/" + registration.getRegistrationId());
+        });
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+        return "oauth2Login";
     }
 
 }
