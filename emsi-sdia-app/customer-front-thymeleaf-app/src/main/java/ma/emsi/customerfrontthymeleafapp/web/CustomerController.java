294b410a6162264bb9e5da0a4af2152915d98f13
Younes-Hounain <younesho9@outlook.com>
2024-01-27 15:43:20 +0100

    [CD] : OAuth working for both Github & Google :lock_with_ink_pen:

--- emsi-sdia-app/customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/web/CustomerController.java
+++ emsi-sdia-app/customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/web/CustomerController.java
@@ -2,9 +2,11 @@ package ma.emsi.customerfrontthymeleafapp.web;
 
 import ma.emsi.customerfrontthymeleafapp.entities.Customer;
 import ma.emsi.customerfrontthymeleafapp.repository.CustomerRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -26,6 +28,15 @@ public class CustomerController {
     @GetMapping(path = "/products")
     public String products(Model model){
         return "products";
+    }
+    @GetMapping(path = "/auth")
+    @ResponseBody
+    public Authentication authentication(Authentication authentication) {
+        return authentication;
+    }
+    @GetMapping(path = "/")
+    public String index() {
+        return "index";
     }
 
 }
